125bd310e1c3d501a1f6f04cb8aaa2364c9dd98f
kooichirooooo <kooichirooooo@gmail.com>
2021-03-12 15:10:17 +0000

    SQL Alchemy and alembic

---
+++ pyprof/entity.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+DB_URL = "postgres+psycopg2://postgres:postgres@localhost/pyprof"
+
+Base = declarative_base()
+
+engine = create_engine(DB_URL, echo=True)
+Session = sessionmaker(bind=engine)
+
+
+class Frame(Base):
+    __tablename__ = "frames"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(32))
+    blocks = relationship("Block", back_populates="frames")
+
+    def __repr__(self) -> str:
+        return f"<Frame(name={self.name})>"
+
+
+class Block(Base):
+    __tablename__ = "blocks"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    frame_id = Column(Integer, ForeignKey("frames.id"))
+    name = Column(String(32))
+
+    def __repr__(self) -> str:
+        return f"<Block(name={self.name})>"
